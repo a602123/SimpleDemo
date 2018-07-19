0abfa59588c727f1120caaefd63d4e1a19af9e2b
师鹏斐 <1176788615@qq.com>
2018-07-19 10:04:05 +0800

    添加初始化项目

---
+++ SimpleDemo.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SimpleDemo.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ SimpleDemo.DataProvider/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SimpleDemo.DataProvider
+{
+    public class Class1
+    {
+    }
+}
